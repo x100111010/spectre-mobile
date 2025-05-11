37191f69a04b4b6c5856b02ac9ed3842732289a7
x100111010 <167847953+x100111010@users.noreply.github.com>
2025-05-11 23:12:10 +0200

    lints

--- lib/widgets/app_simpledialog.dart
+++ lib/widgets/app_simpledialog.dart
@@ -78,7 +78,7 @@ class Dialog extends StatelessWidget {
           borderRadius: BorderRadius.all(Radius.circular(4.0)));
   @override
   Widget build(BuildContext context) {
-    final DialogTheme dialogTheme = DialogTheme.of(context);
+    final dialogTheme = DialogTheme.of(context);
     return AnimatedPadding(
       padding: MediaQuery.of(context).viewInsets +
           const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
